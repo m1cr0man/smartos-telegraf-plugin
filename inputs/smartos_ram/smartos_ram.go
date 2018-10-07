edf02108ee5cb20d98bc169215fbd09a5f646065
Lucas Savva <lucas@m1cr0man.com>
2018-10-07 02:23:47 +0100

    Fix RAM metrics and add totals + arcstat

--- inputs/smartos_ram/smartos_ram.go
+++ inputs/smartos_ram/smartos_ram.go
@@ -3,6 +3,7 @@ package smartos_ram
 import (
 	"log"
 	"os/exec"
+	"regexp"
 	"strconv"
 	"strings"
 
@@ -17,12 +18,13 @@ type SmartOSRAM struct {
 	Global   bool
 	TagAlias bool
 	pgsize   uint64
+	swapsize uint64
 
 	last map[string]uint64
 }
 
 func (s *SmartOSRAM) Description() string {
-	return "Collect global and zone cpu usage"
+	return "Collect global and zone ram usage"
 }
 
 func (s *SmartOSRAM) SampleConfig() string {
@@ -42,22 +44,28 @@ func (s *SmartOSRAM) Gather(acc telegraf.Accumulator) error {
 	stats := token.All()
 
 	pagestats := helpers.FindStat(stats, "unix", -1, "system_pages")
+	arcstats := helpers.FindStat(stats, "zfs", -1, "arcstats")
 	vmstats := helpers.FilterStats(stats, "cpu", -1, "vm")
 
-	pagesFree := helpers.ReadUint(pagestats, "pagesfree") * s.pgsize
+	totalPhysical := helpers.ReadUint(pagestats, "physmem") * s.pgsize
+	pagesFree := helpers.ReadUint(pagestats, "pagesfree")
 	globalFields := map[string]interface{}{
 		// Free page scan rate
 		"nscan": helpers.ReadUint(pagestats, "nscan"),
 		// Pages paged by kernel
 		"pp_kernel":     helpers.ReadUint(pagestats, "pp_kernel"),
 		"pagesfree":     pagesFree,
-		"pagesfree_pct": 100.0 * pagesFree / (helpers.ReadUint(pagestats, "pagestotal")),
+		"pagesfree_pct": 100.0 * pagesFree / helpers.ReadUint(pagestats, "pagestotal"),
 		// Pages paged in/out
 		"pgpgin":  helpers.SumUint(vmstats, "", -1, "", "pgpgin"),
 		"pgpgout": helpers.SumUint(vmstats, "", -1, "", "pgpgout"),
 		// Pages swapped in/out
 		"pgswapin":  helpers.SumUint(vmstats, "", -1, "", "pgswapin"),
 		"pgswapout": helpers.SumUint(vmstats, "", -1, "", "pgswapout"),
+		// Zone overcommit scan activity
+		"zone_cap_scan": helpers.ReadUint(pagestats, "zone_cap_scan"),
+		// ARC size
+		"arcsize": helpers.ReadUint(arcstats, "size"),
 	}
 
 	// Get diff for fault metrics
@@ -71,14 +79,17 @@ func (s *SmartOSRAM) Gather(acc telegraf.Accumulator) error {
 	for _, zone_sample := range helpers.FilterStats(stats, "memory_cap", -1, "") {
 		zoneName := helpers.ReadString(zone_sample, "zonename")
 		fields := map[string]interface{}{}
+		rss := helpers.ReadUint(zone_sample, "rss")
+		swap := helpers.ReadUint(zone_sample, "swap")
+		swapcap := helpers.ReadUint(zone_sample, "swapcap")
+		total := helpers.ReadUint(zone_sample, "physcap")
+		available := total - rss
 
 		if zoneName == "global" {
 			fields = globalFields
+			swapcap = s.swapsize
+			total = totalPhysical
 		}
-
-		rss := helpers.ReadUint(zone_sample, "rss")
-		total := helpers.ReadUint(zone_sample, "physcap")
-		available := total - rss
 
 		// Number of times over allocated memory
 		fields["nover"] = helpers.ReadUint(zone_sample, "nover")
@@ -86,9 +97,10 @@ func (s *SmartOSRAM) Gather(acc telegraf.Accumulator) error {
 		fields["total"] = total
 		fields["available"] = available
 		fields["available_pct"] = 100.0 * available / total
-		fields["swap_percent"] = 100.0 * helpers.ReadUint(zone_sample, "swap") / helpers.ReadUint(zone_sample, "swapcap")
+		fields["swap"] = swap
+		fields["swap_total"] = swapcap
+		fields["swap_percent"] = 100.0 * swap / swapcap
 
-		// TODO arcstats
 		acc.AddFields("ram", fields, map[string]string{
 			"zone": zoneName,
 		})
@@ -105,8 +117,20 @@ func init() {
 	if err != nil {
 		log.Fatal("Failed to get RAM page size: ", err)
 	}
-	newval, _ := strconv.Atoi(strings.Trim(string(out), "\n"))
-	pgsize := uint64(newval)
+	pgsizeRaw, _ := strconv.Atoi(strings.Trim(string(out), "\n"))
+	pgsize := uint64(pgsizeRaw)
 
-	inputs.Add("smartos_ram", func() telegraf.Input { return &SmartOSRAM{pgsize: pgsize, last: map[string]uint64{}} })
+	// Swap size isn't going to change much either
+	out, err = exec.Command("/usr/sbin/swap", "-l").Output()
+	if err != nil {
+		log.Fatal("Failed to get swap size: ", err)
+	}
+	re := regexp.MustCompile(`(\d{4,})\s+\d+[^\d]*$`)
+	match := re.FindAllStringSubmatch(string(out), -1)[0]
+	swapsizeRaw, _ := strconv.Atoi(match[1])
+	swapsize := uint64(swapsizeRaw) * 512
+
+	inputs.Add("smartos_ram", func() telegraf.Input {
+		return &SmartOSRAM{pgsize: pgsize, swapsize: swapsize, last: map[string]uint64{}}
+	})
 }
